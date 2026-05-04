e326cc14f91f2da64b8c4cfa322dbf088c32ee06
Lolita0000 <anarmachadolu@gmail.com>
2026-05-04 20:28:45 -0300

    continuação da estrutura do projeto

--- 03sqla_sync/conf/db_session.py
+++ 03sqla_sync/conf/db_session.py
@@ -10,3 +10,45 @@ from sqlalchemy.future.engine import Engine
 
 from models.model_base import ModelBase
 
+__engine: Optional[Engine] = None
+
+def create_engine(sqlite: bool = False) -> Engine:
+    global __engine
+
+    if __engine:
+        return
+    
+    if sqlite:
+        arquivo_db = 'db/picoles.sqlite'
+        folder = Path(arquivo_db).parent
+        folder.mkdir(parents=True, exist_ok=True)
+
+        conn_str = f'sqlite:///(arquivo_db)'
+        __engine - sa.create_engine(url=conn_str, echo=False, connect_args={"check_same_thread": False})
+    else:
+        conn_str = "mysql:"
+        __engine = sa.create_engine(url=conn_str, echo=False)
+
+    return __engine
+
+def create_session() -> Session:
+    global __engine
+
+    if not __engine:
+        create_engine(sqlite=True)
+
+    __session = sessionmaker(__engine, expire_on_commit=False, class_=Session)
+
+    session: Session = __session()
+
+    return session
+
+def create_tables() -> None:
+    global __engine
+
+    if not __engine
+    create_engine(sqlite=True)
+
+    import models.__all_models
+    ModelBase.metadata.drop_all(__engine)
+    ModelBase.metadata.create_all(__engine)
---
+++ 03sqla_sync/models/aditivo_nutritivo.py
@@ -0,0 +1,14 @@
+import sqlalchemy as sa
+
+from datetime import datetime
+
+from models.model_base import ModelBase
+
+class AdtivoNutritivo(ModelBase):
+    __tabelname__: str = 'adtivos_nutritivos'
+
+    id: int = sa.Column(sa.BigInteger, primary_key=True, autoincrement=True)
+    data_criacao: datetime = sa.Column(sa.DateTime, default=datetime.now, index=True)
+
+    nome: str = sa.Column(sa.String(45), unique=True, nullable=False)
+    formula_quimica: str = sa.Column(sa.String(45), )
